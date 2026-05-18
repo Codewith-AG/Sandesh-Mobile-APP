58f41245fd74ce4616fe9fb2dfb783a291db04c4
Codewith-AG <manag00400@gmail.com>
2026-05-18 15:30:12 +0000

    fix: import flutter/widgets.dart in broadcast service

--- sandesh/lib/services/supabase_broadcast_service.dart
+++ sandesh/lib/services/supabase_broadcast_service.dart
@@ -1,5 +1,6 @@
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart' hide Message;
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'dart:async';
