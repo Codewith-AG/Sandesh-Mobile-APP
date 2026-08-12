d6566e4960dcad680724c5d0d81c0e6522b27ce0
Codewith-AG <Codewith-AG@users.noreply.github.com>
2026-08-12 11:05:41 +0000

    chore: update pubspec.lock with new dependencies (cached_network_image, video_player etc)

--- sandesh/macos/Flutter/GeneratedPluginRegistrant.swift
+++ sandesh/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -24,6 +24,7 @@ import shared_preferences_foundation
 import sqflite_darwin
 import url_launcher_macos
 import video_compress
+import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AgoraRtcNgPlugin.register(with: registry.registrar(forPlugin: "AgoraRtcNgPlugin"))
@@ -45,4 +46,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   VideoCompressPlugin.register(with: registry.registrar(forPlugin: "VideoCompressPlugin"))
+  VideoPlayerPlugin.register(with: registry.registrar(forPlugin: "VideoPlayerPlugin"))
 }
